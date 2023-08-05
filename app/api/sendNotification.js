7c51203912668952c9aebd1730200ac4ad10b717
iisyos <kosuke1216a@yahoo.co.jp>
2023-08-06 00:33:04 +0900

    *feat: env demo

--- app/api/sendNotification.js
+++ app/api/sendNotification.js
@@ -1,5 +1,6 @@
 const api = async (req, res) => {
-  res.status(200).json({ name: import.meta.env.VITE_VAPID_PUBLIC ?? "hoge"});
+  // eslint-disable-next-line no-undef
+  res.status(200).json({ name: process.env.VITE_VAPID_PUBLIC ?? "hoge"});
 };
 
 export default api;
