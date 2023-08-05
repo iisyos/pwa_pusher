3a4d63f031c8c2d99ca875322701923ee7238099
iisyos <kosuke1216a@yahoo.co.jp>
2023-08-06 00:31:56 +0900

    *feat: env demo

--- app/api/sendNotification.js
+++ app/api/sendNotification.js
@@ -1,5 +1,5 @@
 const api = async (req, res) => {
-  res.status(200).json({ name: import.meta.env.VITE_VAPID_PUBLIC });
+  res.status(200).json({ name: import.meta.env.VITE_VAPID_PUBLIC ?? "hoge"});
 };
 
 export default api;
