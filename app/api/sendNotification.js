a0b551d323e0cd896a4aea46d2a23d486c06cc8f
iisyos <kosuke1216a@yahoo.co.jp>
2023-08-06 00:37:12 +0900

    *feat: update api

--- app/api/sendNotification.js
+++ app/api/sendNotification.js
@@ -1,6 +1,23 @@
+/* eslint-disable no-undef */
+import { webPush } from "web-push";
+
+
+webPush.setVapidDetails(
+  'mailto:hoge@example.com',
+  process.env.VITE_VAPID_PUBLIC,
+  process.env.VITE_VAPID_PRIVATE,
+);
+
 const api = async (req, res) => {
-  // eslint-disable-next-line no-undef
-  res.status(200).json({ name: process.env.VITE_VAPID_PUBLIC ?? "hoge"});
+  const subscription = req.body.subscription;
+  const payload = "Hello!";
+  try {
+    await webPush.sendNotification(subscription, payload);
+    res.status(200).json({ message: 'Notification sent.' });
+  } catch (error) {
+    console.error('Error sending notification, reason: ', error);
+    res.sendStatus(500);
+  }
 };
 
 export default api;
